e7dc27b4c11ed65c2432a8082d02f3e618dbc99d
Liliana Barcia <liliana.barcia@accenture.com>
2024-07-23 22:42:03 +0100

    catalog and header components

--- robot-shop/src/app/app.module.ts
+++ robot-shop/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { CatalogComponent } from './catalog/catalog.component';
+import { SiteHeaderComponent } from './site-header/site-header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    CatalogComponent,
+    SiteHeaderComponent
   ],
   imports: [
     BrowserModule,
